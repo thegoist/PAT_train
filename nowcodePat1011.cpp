c549bc5abfa6e1737815d38fa9c2a088191e710b
thegoist <1395214201@qq.com>
2018-10-09 20:52:30 +0800

    练习

--- nowcodePat1011.cpp
+++ nowcodePat1011.cpp
@@ -1,34 +1,36 @@
 #include<stdio.h>
+#include<cstring>
 
 int main(){
 	int num[10] = {0};
-	long long counter = 0;
-	int N,nTemp;
+	char N[1000];
+
 	int n = 0;
 	int temp = 0;
-	while((scanf("%lld",&N) != EOF)){
-		nTemp = N;
-		if(N == 0){
-			num[0]++;
+	while((scanf("%s",N) != EOF)){
+		//初始化计数数组 
+		for(int i = 0 ; i < 10 ; i++){
+			num[i] = 0;
 		}
-		while(nTemp){
-			n++;
-			temp = nTemp%10;
-			nTemp /= 10;
+		n = 0;
+	
+		
+		//处理字符串，计数 
+		for(int i = 0 ; i < strlen(N) ; i++){
+//			printf("%c",N[i]);
+			temp = N[i] - '0';
+//			printf("%d\n",temp);
 			num[temp]++;
 		}
 		
+		//输出 
 		for(int i = 0 ;i<10;i++){
 			if(num[i] != 0){
 				printf("%d:%d\n",i,num[i]); 	
 			}
 		}
 		
-		for(int i = 0 ; i < 10 ; i++){
-			num[i] = 0;
-		}
-		n = 0;
-//		num = {0};
+	
 	}
 	
 	
